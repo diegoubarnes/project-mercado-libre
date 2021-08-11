3d64592cf39825d5460270208f1b4f495c27d76a
Ubarnes <diego.ubarnes@sofka.com.co>
2021-08-11 09:28:09 -0500

    consumo data list

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import api from '../Api';
+import './styles/SearchResults.sass';
 import PageLoading from './PageLoading';
 
 class ListItem extends React.Component {
@@ -28,7 +29,38 @@ class SearchResults extends React.Component {
   state = {
     loading: true,
     error: null,
-    data: undefined,
+    data: [
+      {
+        id: '2de30c42-9deb-40fc-a41f-05e62b5939a7',
+        price: 'Freda',
+        lastName: 'Grady',
+        email: 'Leann_Berge@gmail.com',
+        title: 'Legacy Brand Director',
+        twitter: 'FredaGrady22221-7573',
+        picture:
+          'https://www.gravatar.com/avatar/f63a9c45aca0e7e7de0782a6b1dff40b?d=identicon',
+      },
+      {
+        id: 'd00d3614-101a-44ca-b6c2-0be075aeed3d',
+        price: 'Major',
+        lastName: 'Rodriguez',
+        email: 'Ilene66@hotmail.com',
+        title: 'Human Research Architect',
+        twitter: 'MajorRodriguez61545',
+        picture:
+          'https://www.gravatar.com/avatar/d57a8be8cb9219609905da25d5f3e50a?d=identicon',
+      },
+      {
+        id: '63c03386-33a2-4512-9ac1-354ad7bec5e9',
+        price: 'Daphney',
+        lastName: 'Torphy',
+        email: 'Ron61@hotmail.com',
+        title: 'National Markets Officer',
+        twitter: 'DaphneyTorphy96105',
+        picture:
+          'https://www.gravatar.com/avatar/e74e87d40e55b9ff9791c78892e55cb7?d=identicon',
+      },
+    ],
   };
 
   componentDidMount () {
@@ -45,9 +77,9 @@ class SearchResults extends React.Component {
     this.setState ({loading: true, error: null});
 
     try {
-      const data = await api.Search.searchItems('MLA931241216');
-      console.log(data)
-      this.setState ({loading: false, data: data});
+      // const data = await api.Search.searchItems('MLA931241216');
+      // console.log(data)
+      // this.setState ({loading: false, data: data});
     } catch (error) {
       this.setState ({loading: false, error: error});
     }
@@ -58,6 +90,7 @@ class SearchResults extends React.Component {
     }
 
       return (
+        <div className="Search__container">
         <div className="List">
           <ul className="list-unstyled">
             {this.state.data.map(item => {
@@ -68,6 +101,7 @@ class SearchResults extends React.Component {
               );
             })}
           </ul>
+        </div>
         </div>
       );
   }
